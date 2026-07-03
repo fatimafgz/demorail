f8598838530b1bca51a93eac0cb746e8435bf95b
Piero <pdelgadoc@ulasalle.edu.pe>
2026-07-03 17:27:37 -0500

    inicio

---
+++ src/main/java/com/example/demorail/DemorailApplication.java
@@ -0,0 +1,13 @@
+package com.example.demorail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemorailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemorailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demorail/DemorailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demorail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemorailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
